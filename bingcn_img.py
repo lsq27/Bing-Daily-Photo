c809c2838afa281d4d4ea21c1186cbe9877ed120
chairchan <lushaoqiang27@gmail.com>
2016-10-26 00:43:08 +0800

    function

--- bingcn_img.py
+++ bingcn_img.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''save cn.bing's wallpaper'''
+import os
 import re
 from StringIO import StringIO
 
 import requests
 from PIL import Image
 
-RESPOSE = requests.get('http://cn.bing.com/')
-FIND_URL = re.compile(r'g_img={url: "(\S*.jpg)"')
-FIND_DESCRIBE = re.compile(r'class="sc_light" title="(\S*)')
-IMAGE_URL = FIND_URL.findall(RESPOSE.text)
-DESCRIBE = FIND_DESCRIBE.findall(RESPOSE.text)
-IMAGE = Image.open(StringIO(requests.get(IMAGE_URL[0]).content))
 
-if IMAGE.format == 'JPEG':
-    IMAGE.save(DESCRIBE[0] + '.jpg')
-elif IMAGE.format == 'PNG':
-    IMAGE.save(DESCRIBE[0] + '.png')
+def get_image(url):
+    '''得到图片'''
+    respose = requests.get(url)
+    find_url = re.compile(r'g_img={url: "(\S*.jpg)"')
+    find_describe = re.compile(r'class="sc_light" title="(\S*)')
+    image_url = find_url.findall(respose.text)
+    describe = find_describe.findall(respose.text)
+    image = Image.open(StringIO(requests.get(image_url[0]).content))
+    return (image, describe[0])
+
+
+def save_image(image, describe):
+    if image.format == 'JPEG':
+        image.save(os.path.expanduser('~') + u'/图片/' + describe + '.jpg')
+    elif image.format == 'PNG':
+        image.save(os.path.expanduser('~') + u'/图片/' + describe + '.png')
+
+
+def main():
+    image, describe = get_image('http://cn.bing.com/')
+    save_image(image, describe)
+
 
 if __name__ == '__main__':
-    print 1
+    main()
