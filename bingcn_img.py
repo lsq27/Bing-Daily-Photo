a67069fb540be5d765a81e06a26a0b59cbf2face
chairchan <lushaoqiang27@gmail.com>
2016-11-18 21:00:26 +0800

    add set_wallpaper function for windows and ubuntu's unity

--- bingcn_img.py
+++ bingcn_img.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''save cn.bing's wallpaper'''
+'''save cn.bing.com's wallpaper'''
 import os
+import platform
 import re
+import sys
 from StringIO import StringIO
 
 import requests
 from PIL import Image
 
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-def get_image_describe(url):
-    '''得到图片'''
+
+def get_image_and_describe(url):
+    '''得到图片及描述'''
     respose = requests.get(url)
     find_url = re.compile(r'g_img={url: "(\S*.jpg)"')
     find_describe = re.compile(r'class="sc_light" title="(\S*)')
@@ -22,15 +27,34 @@ def get_image_describe(url):
 
 def save_image(image, describe):
     '''保存图片'''
-    if image.format == 'JPEG':
-        image.save(os.path.expanduser('~') + u'/图片/' + describe + '.jpg')
-    elif image.format == 'PNG':
-        image.save(os.path.expanduser('~') + u'/图片/' + describe + '.png')
+    if os.path.exists(os.path.expanduser('~') + '/图片/' + describe +
+                      '.jpg') is False:
+        if image.format == 'JPEG':
+            image.save(os.path.expanduser('~') + '/图片/' + describe + '.jpg')
+        #elif image.format == 'PNG':
+        #    image.save(os.path.expanduser('~') + '/图片/' + describe + '.png')
+
+
+def set_wallpaper(img_path):
+    '''设置壁纸'''
+    this_platform = platform.system()
+    if this_platform == 'Windows':
+        import win32api
+        import win32con
+        import win32gui
+        win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, img_path,
+                                      win32con.SPIF_SENDCHANGE)
+    elif this_platform == 'Linux':
+        os.system("gsettings set org.gnome.desktop.background picture-uri " +
+                  "'file://" + img_path + "'")
+    print 'success'
 
 
 def main():
-    image, describe = get_image_describe('http://cn.bing.com/')
+    '''main'''
+    image, describe = get_image_and_describe('http://cn.bing.com/')
     save_image(image, describe)
+    set_wallpaper(os.path.expanduser('~') + '/图片/' + describe + '.jpg')
 
 
 if __name__ == '__main__':
